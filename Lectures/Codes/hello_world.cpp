a9e2b7f11aebc2d030faafa4d24ad83d3831cab7
Kashif Ali <74322848+kashiiitech@users.noreply.github.com>
2021-01-27 14:40:18 +0500

    syntax

---
+++ Lectures/Codes/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> // header file
+#include <iostream> // header file
+
+using namespace std; // namespace for cout
+
+int main()
+{
+    printf("Hello World!\n");
+    cout << "hello from cout" << endl;
+    cout << "Another line";
+    return 0;
+}
